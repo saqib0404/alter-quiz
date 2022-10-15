413a50c34dcc6dee1669e993449a0158e739a9cd
Saqib Ahmad <you@example.com>
2022-10-15 10:46:27 +0600

    banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h1 className='text-6xl'>Home</h1>
+            <header>
+                <h2 className='text-4xl'>Enlarge your programming knowledge by solving <span className='text-indigo-500'>Alter Quiz</span></h2>
+            </header>
         </div>
     );
 };
